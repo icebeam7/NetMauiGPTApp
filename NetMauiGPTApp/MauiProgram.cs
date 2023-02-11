abbe3cd388b5ae83d162f1273d8587f4ad1c8276
Daniel Monettelli <danielmonetelli@hotmail.com>
2023-02-10 23:22:06 -0500

    🚀 [Code] Added ChatGPT App UI Challenge s1

--- NetMauiGPTApp/MauiProgram.cs
+++ NetMauiGPTApp/MauiProgram.cs
@@ -1,31 +1,31 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using NetMauiGPTApp.Services;
 using NetMauiGPTApp.ViewModels;
 using NetMauiGPTApp.Views;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace NetMauiGPTApp;
-
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>()
+        .UseSkiaSharp()
+        .ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("Mona-Sans-Bold.ttf", "MonaSansBold");
+            fonts.AddFont("Mona-Sans-Medium.ttf", "MonaSansMedium");
+        }).UseMauiCommunityToolkit();
 
-		builder.Services.AddSingleton<IOpenAIService, OpenAIService>();
-		builder.Services.AddSingleton<ConversationViewModel>();
-		builder.Services.AddSingleton<ConversationView>();
+        builder.Services.AddSingleton<IOpenAIService, OpenAIService>();
+        builder.Services.AddTransient<ConversationViewModel>();
+        builder.Services.AddSingleton<ConversationView>();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
-
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
---
+++ NetMauiGPTApp/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace NetMauiGPTApp.Models
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public bool IsUserMessage { get; set; }
+        public bool IsTextActive { get; set; }
+        public bool IsImageActive { get; set; }
+    }
+}
